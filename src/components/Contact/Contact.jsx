13a6ef826a253b67e683bc7c8dd4b5b03d7d0391
balajiyatheysh <balajiyatheysh87@gmail.com>
2024-06-16 14:28:17 +0530

    my portfolio

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:balajiyatheysh87@gmail.com" target="_blank">balajiyatheysh87@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/balajiyatheysh87/" target="_blank">linkedin.com/balajiyatheysh</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/Balajiyatheysh" target="_blank">github.com/balajiyatheysh</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
